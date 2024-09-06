94165e10f46a3399d7697b3e8c2bb5d849efc777
XuansShi <z3356782747@163.com>
2024-09-06 19:43:16 +0800

    敲桌子案例，每数到7的倍数或者含有7的数字，都要敲桌子而不是报数

---
+++ 敲桌子/敲桌子/敲桌子.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //敲桌子案例
+
+    //1、输出1-100
+
+
+    for (int i = 1; i <= 100; i++)
+    {
+
+        //2、找到特殊数字，打印敲桌子 7的倍数、个位十位有7
+
+        if (i % 7 == 0 || i % 10 == 7 || i / 10 == 7)//如果是特殊数字，打印“敲桌子”
+        {
+            cout << "敲桌子" << endl;
+        }
+        else//如果不是特殊数字，才打印数字
+        {
+            cout << i << endl;
+        }
+
+
+    }
+
+
+    system("pause");
+
+    return 0;
+}
+
