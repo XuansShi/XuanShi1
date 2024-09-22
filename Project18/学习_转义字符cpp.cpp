d58a8a0a7e2f1faeb4d7c685ede31049ea0d05a7
XuanShi <z3356782747@163.com>
2024-09-22 23:31:10 +0800

    学习_转义字符

---
+++ Project18/学习_转义字符cpp.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//转义字符
+	/* 换行符 \n
+	*反斜杠 \\
+	* 水平制表符 \t 作用是整齐地输出数据
+	*
+	*/
+
+	cout << "hello world\n" << endl;
+	cout << "aaa\thelloworld" << endl;
+	cout << "aaaa\thelloworld" << endl;
+
+	system("pause");
+
+	return 0;
+}
