48b4df177188069ca48aabac57df7bc333e84562
XuanShi <z3356782747@163.com>
2024-10-26 21:43:47 +0800

    文件操作-文本文件-写文件

---
+++ Project48/文件操作-文本文件-写文件.cpp
@@ -0,0 +1,35 @@
+//文件操作-文本文件-写文件
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void test01() 
+{
+	//1.包含头文件
+
+	//2.创建流对象
+
+	ofstream ofs;
+
+	//3.指定打开方式
+
+	ofs.open("text.txt", ios::out);
+
+	//4、写内容
+	ofs << "姓名：张三" << endl;
+	ofs << "性别：男" << endl;
+	ofs << "年龄：18" << endl;
+
+
+
+}
+
+
+int main()
+{
+
+	test01();
+
+	system("pause");
+	return 0;
+}
