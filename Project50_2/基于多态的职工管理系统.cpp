042d83d67b9cb0d1b8491e2964bec73761984229
XuanShi <z3356782747@163.com>
2024-11-01 19:58:27 +0800

    基于多态的职工管理系统d2

---
+++ Project50_2/基于多态的职工管理系统.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "workerManager.h"
+
+using namespace std;
+
+int main()
+{
+	//调用成员函数前，先要:
+	 //实例化管理者的对象
+	WorkerManager wm;
+
+	//调用成员函数：
+	wm.Show_Menu();
+
+
+
+
+
+	system("pause");
+	return 0;
+}
